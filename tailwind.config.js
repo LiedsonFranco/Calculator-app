fee7a595dfb87faf912fe0f18e94fd55f640b5f1
Liedson Franco <126687225+LiedsonFranco@users.noreply.github.com>
2024-03-26 22:20:50 +0000

    day 1 fineshed

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+           'very-dark-desaturated-blue': 'hsl(222, 26%, 31%)',
+           'very-dark-desaturated-keypad': 'hsl(223, 31%, 20%)',
+           'very-dark-desaturated-screen': 'hsl(224, 36%, 15%)',
+           'light-grayish-orange': 'hsl(30, 25%, 89%)',
+           'very-dark-grayish-blue': 'hsl(221, 14%, 31%)',
+           'red':'hsl(6, 63%, 50%)',
+           'desaturated-dark-blue-key': 'hsl(225, 21%, 49%)',
+        }
+      }
+    }
+  }
